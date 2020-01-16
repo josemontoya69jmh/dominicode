0d5ae4ebadc15c279fdc0ac43b2ea2fff492a753
josemontoya69jmh <2017josemontoya@gmail.com>
2020-01-16 13:44:27 +0100

    Sin Errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { DetailsBookComponent } from './components/details-book/details-book.component';
+import { ListBookComponent } from './components/admin/list-book/list-book.component';
+import { LoginComponent } from 'src/app/components/users/login/login.component';
+import { RegisterComponent } from 'src/app/components/users/register/register.component';
+import { ProfileComponent } from 'src/app/components/users/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
+import { AuthGuard } from './guards/auth.guard';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'offers', component: OffersComponent, canActivate: [AuthGuard] },
+  { path: 'book/:id', component: DetailsBookComponent },
+  { path: 'admin/list-books', component: ListBookComponent, canActivate: [AuthGuard] },
+  { path: 'user/login', component: LoginComponent },
+  { path: 'user/register', component: RegisterComponent },
+  { path: 'user/profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '**', component: Page404Component }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListBookComponent } from './components/admin/list-book/list-book.component';
+import { DetailsBookComponent } from './components/details-book/details-book.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './components/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+
+
+import { Page404Component } from './components/page404/page404.component';
+import { RegisterComponent } from './components/users/register/register.component';
+// Este componente es imprescindible para trabajar con ngModule
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBookComponent,
+    DetailsBookComponent,
+    HeroComponent,
+    HomeComponent,
+    ModalComponent,
+    NavbarComponent,
+    OffersComponent,
+    LoginComponent,
+    ProfileComponent,
+    Page404Component,
+    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
