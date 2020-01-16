c75e5dbef158a60e37e89f66cb60a8ec76a3a27c
josemontoya69jmh <2017josemontoya@gmail.com>
2020-01-16 21:02:31 +0100

    termi

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+// en el constructor metemos el afAuth y el router
+  constructor(public afAuth: AngularFireAuth, private router: Router, private authService: AuthService) { }
+  public email: string = '';
+  public password: string = '';
   ngOnInit() {
   }
 
+  onLogin(): void {
+    // this.authService.loginEmailUser(this.email, this.password)
+    //   .then((res) => {
+    //     this.onLoginRedirect();
+    //   }).catch(err => console.log('err', err.message));
+  }
+
+  onLoginGoogle() {
+    // Con esta funcion nos logeamos y nos redirecciona, hay que meterlo por parametros en el constructor
+    // esto nos levanta un popup
+this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+// nos redirecciona
+this.router.navigate(['admin/list-books']);
+
+  }
+
+
+  onLogout() {
+    this.authService.logoutUser();
+  }
+  onLoginRedirect(): void {
+    this.router.navigate(['admin/list-books']);
+  }
 }
